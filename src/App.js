53d5c1f2e569d51e0053a9efaaa9ac728f28b9d3
Brian <brian12leonard@gmail.com>
2024-09-08 10:15:51 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NasaImage from './components/nasaImage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NasaImage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
